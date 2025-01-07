cf0cec02d77cce2bb15581e316ad17c52309188d
Madsbert <mberthelsen@live.dk>
2025-01-07 13:00:40 +0100

    small delete

--- src/main/java/org/example/kittyfy/HelloController.java
+++ src/main/java/org/example/kittyfy/HelloController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
-import javafx.scene.
+
 
 
 
